2de9858ab0a32476cef91f513d6a2bd858eb5daf
Colin Roberts <colin@primitive.xyz>
2024-04-22 17:31:20 -0600

    remove namespacing

--- src/ECC/Basic.lean
+++ src/ECC/Basic.lean
@@ -10,9 +10,6 @@ import Mathlib.Tactic.Group
 import Mathlib.GroupTheory.Coprod.Basic
 import Mathlib.Algebra.Group.Defs
 
-namespace EllipticCurve
-
-
 def ECPoints {k : Type} [Field k] [Fintype k] (E : EllipticCurve k) : Type := E.toAffine.Point
 
 noncomputable instance ECPointsCommGroup {k : Type} [Field k] [Fintype k] (E : EllipticCurve k) : AddCommGroup (ECPoints E) :=
@@ -29,14 +26,11 @@ noncomputable instance ECPointsGroup {k : Type} [Field k] [Fintype k] (E : Ellip
     mul_left_inv := AddCommGroup.toAddGroup.add_left_neg,
   }
 
-end EllipticCurve
-
 def IsProductOfTwoCyclicGroups (G : Type*) [Group G] : Prop :=
   ∃ (H K : Subgroup G), IsCyclic H ∧ IsCyclic K ∧ Nontrivial H ∧ Nontrivial K ∧ Nonempty (G ≃* H.prod K)
 
-
-theorem point_group_is_cyclic_or_product_of_cyclic {k : Type} [Field k] [Fintype k] (E : EllipticCurve k) [Group (ECPoints)] :
-  IsCyclic (ECPoints) ∨ IsProductOfTwoCyclicGroups (ECPoints) := by
+theorem point_group_is_cyclic_or_product_of_cyclic {k : Type} [Field k] [Fintype k] (E : EllipticCurve k) [Group (ECPoints E)] :
+  IsCyclic (ECPoints E) ∨ IsProductOfTwoCyclicGroups (ECPoints E) := by
   sorry
 
 
